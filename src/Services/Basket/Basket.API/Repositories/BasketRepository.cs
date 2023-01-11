a7adcfbef6d834aa44f9ba8257219ab2e8deb3ad
Afonso Cardoso <afonsocardoso@ua.pt>
2023-01-11 12:08:36 +0000

    basket + catalog microservices developed

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,45 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Threading.Tasks;
+using System;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        //Below are the CRUD operations
+        //GET all the products from the basket
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            //use JSONconvert class to save and extract json objects from redis cache db
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        //POST products to the basket
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+
+        //DELETE the basket and all the products
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
